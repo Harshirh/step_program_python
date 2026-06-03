3fe1f7dd6b2d73dcd563bf8aca6406f3c2816f64
Harshirh <velichelamalaharshith@gmail.com>
2026-06-03 10:09:04 +0530

    two_pointers and Conditional_loops doone

---
+++ python_intermediate_topics/conditional.py
@@ -0,0 +1,9 @@
+# person_name = str(input("Enter the person's name: "))
+# age = int(input("Enter the person's age: "))
+
+# if age < 18:
+#     print(f'{person_name} is a minor.')
+# elif age >= 18 and age < 65:
+#     print(f'{person_name} is an adult.')
+# else:
+#     print(f'{person_name} is a senior citizen.')
